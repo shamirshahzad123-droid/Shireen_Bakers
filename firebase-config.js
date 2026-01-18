c61de332860c0a24ddcabad5aa62fa383f6bb467
shamirshahzad123-droid <shamirshahzad123@gmail.com>
2026-01-19 04:03:55 +0500

    test

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,12 @@
 
 const firebaseConfig = {
     apiKey: "AIzaSyBmmeIKJgEMkzhqR4heqj4bp-wfbzOpjpU",
-    authDomain: "shireen-bakers.com", // <-- Corrected
-    databaseURL: "https://bakery-website-efebd.firebaseio.com", // <-- Added (verify actual value from console)
+    authDomain: "bakery-website-efebd.firebaseapp.com",
     projectId: "bakery-website-efebd",
     storageBucket: "bakery-website-efebd.firebasestorage.app",
     messagingSenderId: "235221776824",
     appId: "1:235221776824:web:5e0102dc71ee75da8c374d",
-    measurementId: "G-Q51VD2HSV8" // <-- Verify this is correct for your GA4 property
+    measurementId: "G-Q51VD2HSV8"
 };
 
 // Initialize Firebase (compat mode)
